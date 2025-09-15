05108f8f85d8939522b5c690e0d2c8b60a083d4d
宽望 <leichao.lc@antgroup.com>
2025-09-15 11:50:48 +0800

    PullRequest: 373 kv_output_aggregator support heterogeneous 23917
    Merge branch kw/dev-v0.10.2.915 of git@code.alipay.com:Theta/vllm.git into dev-v0.10.2.915
    https://code.alipay.com/Theta/vllm/pull_requests/373
    
    Reviewed-by: 沧濯 <zhengshoujian.zsj@antgroup.com>
    
    
    * kv_output_aggregator support heterogeneous 23917

--- vllm/v1/executor/multiproc_executor.py
+++ vllm/v1/executor/multiproc_executor.py
@@ -25,7 +25,6 @@ from vllm.distributed import (destroy_distributed_environment,
                               destroy_model_parallel)
 from vllm.distributed.device_communicators.shm_broadcast import (Handle,
                                                                  MessageQueue)
-from vllm.distributed.kv_transfer.kv_connector.utils import KVOutputAggregator
 from vllm.distributed.parallel_state import (get_dp_group, get_ep_group,
                                              get_pp_group, get_tp_group)
 from vllm.executor.multiproc_worker_utils import (
@@ -127,8 +126,6 @@ class MultiprocExecutor(Executor):
 
         self.output_rank = self._get_output_rank()
         self.has_connector = self.vllm_config.kv_transfer_config is not None
-        self.kv_output_aggregator = KVOutputAggregator(
-            self.parallel_config.world_size)
 
     def start_worker_monitor(self):
         workers = self.workers
